68e601c2e26c6ea8ab028bb9b9cb04b13c1ce80c
sarthag <asarthak2002@gmail.com>
2021-10-09 18:43:58 +0530

    Section 2 done, some gapos present

---
+++ Constitution Code/Article2/section2.pl
@@ -0,0 +1,25 @@
+power(president, grant(reprieve(offenseAgainstUS))) :- \+ (impeachment).
+power(president, grant(pardon(offenseAgainstUS))) :- \+ (impeachment).
+
+power(president, makeTreaties) :- adviceAndConsent(senate).
+power(president, appoint(nominate(ambassador))) :- adviceAndConsent(senate).
+power(president, appoint(nominate(minister))) :- adviceAndConsent(senate).
+power(president, appoint(nominate(consul))) :- adviceAndConsent(senate).
+power(president, appoint(nominate(judge(supremeCourt)))) :- adviceAndConsent(senate).
+
+/*Appoint Officers*/
+
+power(president, grantCommissions) :- recess(senate).
+/*Commisions shall expire*/
+
+
+
+commanderInChiefOfArmy(president).
+commanderInChiefOfNavyy(president).
+commanderInChiefOfMilitia(president) :- inService(militia).
+
+
+/* Requieres opinion of respective Head of Departments*/
+
+power(principalOfficer(Department), advice(president)).
+
