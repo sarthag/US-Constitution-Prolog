799b7dbcc7eeb2be796a34cb4adb84ba937b15ce
EpichilFire3 <91728278+EpichilFire3@users.noreply.github.com>
2021-10-14 00:23:27 +0530

    Create Section2.pl

---
+++ Constitution Code/Article3/Section2.pl
@@ -0,0 +1,23 @@
+judicialPowersExtent :- caseOf.
+caseOf :- 
+law(constitution);
+equity(constitution);
+law(unitedStates);
+treaty(unitedStates,X).
+caseOf :- 
+ambassador;
+publicMinister;
+consul.
+caseOf :-
+admiraltyJurisdiction;
+maritimeJurisdiction.
+caseOf :- controversy(X,Y).
+controversy(X,Y) :- 
+controversy(state(State1),state(State2));
+controversy(state(State1),citizen(State2));
+controversy(citizen(State1),citizen(State2));
+controversy(citizen1(State1),citizen2(State1), 
+claimLandUnderGrant(citizen1(State1),State2),
+claimLandUnderGrant(citizen1(State1),State3);
+controversy(foreignState, citizen(State));
+controversy(foerignState, state(State1).
