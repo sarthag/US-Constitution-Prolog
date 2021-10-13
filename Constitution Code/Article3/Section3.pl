53c3148a709d04e3f2f6223f4e40ce7fafa0e68c
EpichilFire3 <91728278+EpichilFire3@users.noreply.github.com>
2021-10-14 00:49:06 +0530

    Create Section3.pl

---
+++ Constitution Code/Article3/Section3.pl
@@ -0,0 +1,6 @@
+treason(Actor1,unitedStates) :- 
+levyWar(Actor1, unitedStates);
+giveAidAndComfort(Actor1,Actor2), enemy(Actor2,unitedStates).
+convicted(treason(Actor,unitedStates)) :-
+testimony((Witness1, Act1),(Witness2, Act1));
+confession(Actor,Act).
