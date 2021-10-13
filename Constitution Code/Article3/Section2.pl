1323365a433eb4e585f1685ffca9a901a91ba25d
EpichilFire3 <91728278+EpichilFire3@users.noreply.github.com>
2021-10-14 01:06:45 +0530

    Update Section2.pl

--- Constitution Code/Article3/Section2.pl
+++ Constitution Code/Article3/Section2.pl
@@ -1,23 +1,26 @@
-judicialPowersExtent :- caseOf.
-caseOf :- 
+judicialPowersExtent :- caseOf1; caseOf2; caseOf3; caseOf4.
+caseOf1 :- 
   law(constitution);
   equity(constitution);
   law(unitedStates);
   treaty(unitedStates,X).
-caseOf :- 
+caseOf2 :- 
   ambassador;
   publicMinister;
   consul.
-caseOf :-
+caseOf3 :-
   admiraltyJurisdiction;
   maritimeJurisdiction.
-caseOf :- controversy(X,Y).
-controversy(X,Y) :- 
-  controversy(state(State1),state(State2));
-  controversy(state(State1),citizen(State2));
-  controversy(citizen(State1),citizen(State2));
-  controversy(citizen1(State1),citizen2(State1), 
-  claimLandUnderGrant(citizen1(State1),State2),
-  claimLandUnderGrant(citizen1(State1),State3);
-  controversy(foreignState, citizen(State1));
-  controversy(foerignState, state(State1).
+caseOf4 :- controversy1(X,Y), controversy2(X,Y).
+controversy1(X,Y) :- 
+  controversy1(state(State1),state(State2));
+  controversy1(state(State1),citizen(State2));
+  controversy1(foerignState, state(State1).
+ controversy2(X,Y) :- 
+    controversy2(citizen(State1),citizen(State2));
+    controversy2(citizen1(State1),citizen2(State1), 
+    claimLandUnderGrant(citizen1(State1),State2),
+    claimLandUnderGrant(citizen1(State1),State3);
+    controversy2(foreignState, citizen(State1)).
+originalJurisdiction(supremeCourt) :- caseOf1; controversy1(X,Y).
+appellateJurisdiction(supremeCourt) :- caseOf2; caseOf3; controversy2(X,Y).
