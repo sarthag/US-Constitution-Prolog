8e456dafddb6e11c3b1ab5e363cc11fe24e58bd3
Shaz Furniturewala <72245851+shortguyshaz@users.noreply.github.com>
2021-10-17 18:33:18 +0530

    Add files via upload

---
+++ Constitution Code/Amendments/Amendment15.pl
@@ -0,0 +1,9 @@
+/* SECTION 1 */
+
+not(denied(rights(citizen(StateA), basedOn(race)))).
+not(denied(rights(citizen(StateA), basedOn(color)))).
+not(denied(rights(citizen(StateA), basedOn(previousConditionOfServitude)))).
+
+/* SECTION 2 */
+
+power(congress, enforceArticle(legislation))
