b3a3a29ad1964b41b81daaa7846d78ea9586b118
sarthag <asarthak2002@gmail.com>
2021-10-17 16:27:15 +0530

    Outline of final doc

---
+++ constitution.pl
@@ -0,0 +1,323 @@
+/*
+NAME: Anshika Gupta
+ID: 2020A7PS0111P
+
+NAME: Sahil Shah
+ID: 2020A7PS0140P
+
+NAME: Sarthak Agarwal
+ID: 2020A7PS0112P
+
+NAME: Shaz Furniturewala
+ID: 2020A7PS0025P
+
+NAME: Shreyas Sesham
+ID: 2020A7PS1684P
+*/
+
+
+/* Test Case Facts */
+
+age(rohan, 23).
+citizen(rohan, 23).
+age(meera, 30).
+citizen(meera, 8).
+age(david, 35).
+citizen(david, 35).
+age(leonard, 40).
+citizen(leonard, 40).
+age(amy, 38).
+citizen(amy, 5).
+
+stateOfUS(newHampshire).
+stateOfUS(massachusetts).
+stateOfUS(connecticut).
+stateOfUS(newYork).
+stateOfUS(newJersey).
+stateOfUS(pennsylvania).
+stateOfUS(delaware).
+stateOfUS(maryland).
+stateOfUS(virginia).
+stateOfUS(northCarolina).
+stateOfUS(southCarolina).
+stateOfUS(georgia).
+
+monday(7, 1, 2019).
+monday(2, 12, 2019).
+
+
+/* Article 1 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+/* Section 3 */
+
+
+/* Section 4 */
+
+
+/* Section 5 */
+
+
+/* Section 6 */
+
+
+/* Section 7 */
+
+
+/* Section 8 */
+
+
+/* Section 9 */
+
+
+/* Section 10 */
+
+
+
+
+/* Article 2 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+/* Section 3 */
+
+
+/* Section 4 */
+
+
+
+
+/* Article 3 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+/* Section 3 */
+
+
+
+
+/* Article 4 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+/* Section 3 */
+
+
+/* Section 4 */
+
+
+
+/* Article 5 */
+
+
+
+/* Article 6 */
+
+
+
+/* Article 7 */
+
+
+
+/* Amendments */
+
+/* Amendment 1 */
+
+
+/* Amendment 2 */
+
+
+/* Amendment 3 */
+
+
+
+/* Amendment 4 */
+
+
+
+/* Amendment 5 */
+
+
+/* Amendment 6 */
+
+
+/* Amendment 7 */
+
+
+
+/* Amendment 8 */
+
+
+/* Amendment 9 */
+
+
+/* Amendment 10 */
+
+
+/* Amendment 11 */
+
+
+/* Amendment 12 */
+
+
+/* Amendment 13 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+
+/* Amendment 14 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+/* Section 3 */
+
+
+/* Section 4 */
+
+
+/* Section 5 */
+
+
+
+/* Amendment 15 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+
+/* Amendment 16 *
+
+
+
+/* Amendment 17 */
+
+
+
+/* Amendment 18 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+/* Section 3 */
+
+
+
+/* Amendment 19 */
+
+
+
+/* Amendment 20 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+/* Section 3 */
+
+
+/* Section 4 */
+
+
+/* Section 5 */
+
+
+/* Section 6 */
+
+
+/* Amendment 21 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+/* Section 3 */
+
+
+
+/* Amendment 22 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+
+/* Amendment 23 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+
+/* Amendment 24 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+
+/* Amendment 25 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+/* Section 3 */
+
+
+/* Section 4 */
+
+
+
+/* Amendment 26 */
+
+/* Section 1 */
+
+
+/* Section 2 */
+
+
+
+/* Amendment 27 */
