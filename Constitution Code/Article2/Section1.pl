903702c288ca8e70ae4632767b69618defdc4326
sarthag <asarthak2002@gmail.com>
2021-10-17 19:34:09 +0530

    updates

--- Constitution Code/Article2/Section1.pl
+++ Constitution Code/Article2/Section1.pl
@@ -1,9 +1,23 @@
 age(Name, Age).
 citizen(Name, Years).
+resident(Name, stateOfUS(X)).
+elector(Name, stateOfUS(Residence), stateOfUS(Standing)) :-
+    resident(Name, stateOfUS(Residence)).
 
 executivePower(president).
-/* Term of Power */
+term(president, 4).
 
+numElectors(stateOfUS(X), Electors) :- 
+    numSenators(stateOfUS(X), Senators),
+    numRepresentatives(stateOfUS(X), Representatives),
+    (Electors = Senators + Representatives).
+ 
+elector(Name, _, _) :- not(holdingOffice(Name)).
+
+voteByBallot(Elector, Person1, Person2) :- 
+    elector(Elector, _, stateOfUS(Standing)),
+    (resident(Person1, stateOfUS(not(Standing)));
+     resident(Person2, stateOfUS(not(Standing)))).
  
 /*
 Elections
