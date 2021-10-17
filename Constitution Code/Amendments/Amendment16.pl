394f8361d1e05296ca6ee138f6e1fa91f1655b05
Shaz Furniturewala <72245851+shortguyshaz@users.noreply.github.com>
2021-10-17 18:40:06 +0530

    Add files via upload

---
+++ Constitution Code/Amendments/Amendment16.pl
@@ -0,0 +1,4 @@
+power(congress, withoutRegardTo(source, layOrCollect(taxes))).
+power(congress, withoutRegardTo(apportionmentAmongStates, layOrCollect(taxes))).
+power(congress, withoutRegardTo(census, layOrCollect(taxes))).
+power(congress, withoutRegardTo(enumeration, layOrCollect(taxes))).
