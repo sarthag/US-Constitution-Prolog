2ce36476ff6ae75da11a1ae20884b3d6dfc87459
EpichilFire3 <91728278+EpichilFire3@users.noreply.github.com>
2021-10-17 19:11:12 +0530

    Update section2.pl

--- Constitution Code/Aritcle1/section2.pl
+++ Constitution Code/Aritcle1/section2.pl
@@ -1,4 +1,4 @@
 age(Name, Age).
 citizen(Name, Years).
-
 qualified(X, houseOfRepresentatives) :- (age(X, Age), Age >= 25), (citizen(X, Years), Years >= 7).
+voterQualified(Y, positionOfOffice) :- (age(Y, Age), Age >= 18). %Ammendment 26%
