2ac0f03b4c4720831567ae7f5f0b6114d4a9dd32
EpichilFire3 <91728278+EpichilFire3@users.noreply.github.com>
2021-10-19 20:09:05 +0530

    Update article-7.pl

--- Constitution Code/Article7/article-7.pl
+++ Constitution Code/Article7/article-7.pl
@@ -2,42 +2,41 @@ establishmentOfConstitution(nineStates) :-
     ratification(conventions(nineStates)).
 dateOfRatification(17/09/1787).
 
-witness(State,Name).
-witness(New Hampshire, John Langdon).
-witness(New Hampshire, Nicholas Gilman).
-witness(Massachusetts, Nathaniel Gorham).
-witness(Connecticut, Wm. Saml. Johnson).
-witness(Connecticut,Roger Sherman).
-witness(New York, Alexander Hamilton).
-witness(New Jersey, Wil: Livingston).
-witness(New Jersey, David Brearley).
-witness(New Jersey, Wm. Paterson).
-witness(New Jersey, Jona: Dayton).
-witness(Pennsylvania, B Franklin).
-witness(Pennsylvania, Thomas Mifflin).
-witness(Pennsylvania, Robt Morris).
-witness(Pennsylvania, Geo. Clymer).
-witness(Pennsylvania, Thos. FitzSimons).
-witness(Pennsylvania, Jared Ingersoll).
-witness(Pennsylvania, James Wilson).
-witness(Pennsylvania, Gouv Morris).
-witness(Delaware, Geo: Read).
-witness(Delaware, Gunning Bedford jun).
-witness(Delaware, John Dickinson).
-witness(Delaware, Richard Bassett).
-witness(Delaware, Jaco: Broom).
-witness(Maryland, James McHenry).
-witness(Maryland, Dan of St. Thos. Jenifer).
-witness(Maryland, Danl Carroll).
-witness(Virginia, John Blair).
-witness(Virginia, James Madison Jr.).
-witness(North Carolina, Wm. Blount).
-witness(North Carolina, Richd. Dobbs Spaight).
-witness(North Carolina, Hu Williamson).
-witness(South Carolina, J. Rutledge).
-witness(South Carolina, Charles Cotesworth Pinckney).
-witness(South Carolina, Charles Pinckney).
-witness(South Carolina, Pierce Butler).
-witness(Georgia, William Few).
-witness(Georgia, Abr Baldwin).
-witness(Washington, Secretary William Jackson).
+witness(newHampshire, johnLangdon).
+witness(newHampshire, nicholasGilman).
+witness(massachusetts, nathanielGorham).
+witness(connecticut, wmSamlJohnson).
+witness(connecticut, rogerSherman).
+witness(newYork, alexanderHamilton).
+witness(newJersey, wil:Livingston).
+witness(newJersey, davidBrearley).
+witness(newJersey, wmPaterson).
+witness(newJersey, jona:Dayton).
+witness(pennsylvania, bFranklin).
+witness(pennsylvania, thomasMifflin).
+witness(pennsylvania, robtMorris).
+witness(pennsylvania, geo.Clymer).
+witness(pennsylvania, thos.FitzSimons).
+witness(pennsylvania, jaredIngersoll).
+witness(pennsylvania, jamesWilson).
+witness(pennsylvania, gouvMorris).
+witness(delaware, geo:Read).
+witness(delaware, gunningBedfordJun).
+witness(delaware, johnDickinson).
+witness(delaware, richardBassett).
+witness(delaware, jaco:Broom).
+witness(maryland, jamesMcHenry).
+witness(maryland, danOfSt.Thos.Jenifer).
+witness(maryland, danlCarroll).
+witness(virginia, johnBlair).
+witness(virginia, jamesMadison Jr.).
+witness(northCarolina, wmBlount).
+witness(northCarolina, richdDobbsSpaight).
+witness(northCarolina, huWilliamson).
+witness(southCarolina, j.Rutledge).
+witness(southCarolina, charlesCotesworthPinckney).
+witness(southCarolina, charlesPinckney).
+witness(southCarolina, pierceButler).
+witness(georgia, williamFew).
+witness(georgia, abrBaldwin).
+witness(washington, secretaryWilliamJackson).
