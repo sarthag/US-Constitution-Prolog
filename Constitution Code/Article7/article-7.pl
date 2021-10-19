371c8461dfce4d4d7dab60a45d8e683521cf6dfd
EpichilFire3 <91728278+EpichilFire3@users.noreply.github.com>
2021-10-19 19:45:27 +0530

    Update article-7.pl

--- Constitution Code/Article7/article-7.pl
+++ Constitution Code/Article7/article-7.pl
@@ -1,3 +1,30 @@
-establishmentOfConstitution(states) :-
+establishmentOfConstitution(nineStates) :-
     ratification(conventions(nineStates)).
-
+dateOfRatification(17/09/1787).
+witness(State,Name).
+witness(New Hampshire, John Langdon).
+witness(New Hampshire, Nicholas Gilman).
+witness(Massachusetts, Nathaniel Gorham).
+witness(Connecticut, Wm. Saml. Johnson).
+witness(Connecticut,Roger Sherman).
+witness(New York, Alexander Hamilton).
+witness(New Jersey, Wil: Livingston).
+witness(New Jersey, David Brearley).
+witness(New Jersey, Wm. Paterson).
+witness(New Jersey, Jona: Dayton).
+witness(Pennsylvania, B Franklin).
+witness(Pennsylvania, Thomas Mifflin).
+witness(Pennsylvania, Robt Morris).
+witness(Pennsylvania, Geo. Clymer).
+witness(Pennsylvania, Thos. FitzSimons).
+witness(Pennsylvania, Jared Ingersoll).
+witness(Pennsylvania, James Wilson).
+witness(Pennsylvania, Gouv Morris).
+witness(Delaware, Geo: Read).
+witness(Delaware, Gunning Bedford jun).
+witness(Delaware, John Dickinson).
+witness(Delaware, Richard Bassett).
+witness(Delaware, Jaco: Broom).
+witness(Maryland, James McHenry).
+witness(Maryland, Dan of St. Thos. Jenifer).
+witness(Maryland, Danl Carroll ).
