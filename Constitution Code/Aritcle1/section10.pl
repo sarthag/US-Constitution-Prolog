26c854695fda40962172e72e150eb49695ae0264
shreyas1209 <shreyas.sesham12@gmail.com>
2021-10-20 23:22:50 +0530

    Finished section10 article 1

---
+++ Constitution Code/Aritcle1/section10.pl
@@ -0,0 +1,31 @@
+tenderList([gold,silver]).
+paymentOfDebts(Mode) :- not(member(Mode,tenderList)).
+billsNotAllowed([billOfAttainder,exPostFactoLaw,impairingObligationOfContracts,grant(titleOfNobility)]).
+/*passBill is for passing disallowed bills*/
+cantPassBill(Bill) :- member(Bill,billsNotAllowed)
+notAllowedForStates([entering([Treaty,Alliance,Confederation]),
+grant(lettersOfMarqueeAndReprisal),
+coinMoney,
+emit(billsOfCredit),
+tender(paymentOfDebts(Mode)),
+cantPassBill(Bill)]).
+
+/* Functor tells whether a certain action is allowed or not*/
+dissallowed(Action) :- member(Action,memberOfStates).
+
+/* List of items that require consent of congress*/
+needConsentOfCongress ([
+    lay(impostsOrDuties(importsOrExports)),
+    lay(dutyOfTonnage),
+    keep(troops),
+    keep(shipsOfWarduringPeace),
+    enter(agreementOrCompact([anotherState,foriegnPower])),
+    engage(war(Reason)).
+]).
+/* noConsentNeeded is a list of conditions where there is no necessity for Congress consent for war*/
+noConsentNeeded([invaded,imminentDanger]).
+/* need consent unless invasion or imminent danger*/
+engage(war(Reason)) :- not(member(Reason,noConsentNeeded)).
+
+/* Functor tells whether a certain action needs Congress consent*/
+needsCongressConsent(Action) :- member(Action,needConsentOfCongress).
