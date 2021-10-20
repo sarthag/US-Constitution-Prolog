1af989d5064f9f671a491076e884ed2719404628
EpichilFire3 <91728278+EpichilFire3@users.noreply.github.com>
2021-10-20 23:59:52 +0530

    Update section3.pl

--- Constitution Code/Article2/section3.pl
+++ Constitution Code/Article2/section3.pl
@@ -1,5 +1,10 @@
-information(president, congress, state(union)) :- recommend()
-
-/*
-No clue
-*\
+information(president, congress, stateOfUnion).
+recommend(president, congress, measures(X,Y) :- measures(necessary,expedient).
+convene(president,houseOfRepresentatives,senate) :- occasion(extraordinary).
+convene(president,X) :- 
+  occasion(extraordinary),
+  X = houseOfRepresentatives; X = senate.
+adjourn(president, houseOfrepresentatives, senate) :- disagreement(houseOfrepresentatives, senate).
+receive(president,X) :-
+  X = ambassador(ForeignState), X = publicMinister(ForeignState).
+  commission(president, officerOfUnitedStates).
