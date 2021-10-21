b8d6dcbb638c6ea5e6cf1a8e8e5ca4db1f670dcc
half-anerd <74734120+half-anerd@users.noreply.github.com>
2021-10-22 03:16:08 +0530

    Update Amendment10.pl

--- Constitution Code/Amendments/Amendment10.pl
+++ Constitution Code/Amendments/Amendment10.pl
@@ -1,3 +1,6 @@
-power(State, X) :-
+power(stateOfUS(Y), X) :-
     not(delegated(X, unitedStates, constitution)),
-    not(prohibited(X, State, constitution)).
+    not(prohibited(X, stateOfUS(Y), constitution)).
+    
+    
+*/ will throw error for power(X,Y), will not for power(congress, Y) */
