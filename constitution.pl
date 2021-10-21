18d64195df64fb4d5abfe63c530c189e4324edff
sarthag <asarthak2002@gmail.com>
2021-10-21 23:39:56 +0530

    article 1 section 5 done

--- constitution.pl
+++ constitution.pl
@@ -75,7 +75,28 @@ congress(X) :- member(X,[senate,houseOfRepresentatives]).
 
 
 /* Section 5 */
+concurrence(house,Fraction).
+senate.
+houseOfRepresentatives.
+house :- senate.
+house :- houseOfRepresentatives.
 
+disorderlyBehaviour(X).
+power(house,judge(elections(members))).
+power(house,returnsAndQualifications(members)).
+power(house,compel(attendance(absent(members)))).
+
+power(house,determine(rulesOfProceedings)).
+punish(member).
+punish(member,Fraction) :- disorderlyBehaviour(member),(concurrence(house,Fraction ), Fraction > 2/3).
+power(house,punish(member)):- disorderlyBehaviour(member).
+
+keep(house,journalOfProceedings).
+includeYeasAndNays(members(house),Fraction) :- (concurrence(house, Fraction),Fraction > (1/5)).
+
+content(journalOfProceedings,Fraction) :- includeYeasAndNays(members(house),Fraction) .
+
+adjourn(house,TimeOfAdjourning) :- (TimeOfAdjourning =< 3).
 
 /* Section 6 */
 
