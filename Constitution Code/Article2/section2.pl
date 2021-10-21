dcc3a18beea5a9fd8a2ce4309657a41ac2e275a9
sarthag <asarthak2002@gmail.com>
2021-10-22 02:40:03 +0530

    article 2 section 2

--- Constitution Code/Article2/section2.pl
+++ Constitution Code/Article2/section2.pl
@@ -1,23 +1,21 @@
-power(president, grant(reprieve(offenseAgainstUS))) :- not(impeachment).
-power(president, grant(pardon(offenseAgainstUS))) :- not(impeachment).
+power(president, grant(reprieve(offenseAgainstUS))).
+power(president, grant(pardon(offenseAgainstUS))).
 
+adviceAndConsent(senate).
 power(president, makeTreaties) :- adviceAndConsent(senate).
-power(president, appoint(nominate(ambassador))) :- adviceAndConsent(senate).
-power(president, appoint(nominate(minister))) :- adviceAndConsent(senate).
-power(president, appoint(nominate(consul))) :- adviceAndConsent(senate).
-power(president, appoint(nominate(judge(supremeCourt)))) :- adviceAndConsent(senate).
+power(president, appoint(nominate(X))) :-
+    member(X, [ambassador, minister, consul, judge(supremeCourt)]),
+    adviceAndConsent(senate).
 
-/*Appoint Officers*/
+power(president, appoint(officers)).
 
+recess(senate).
 power(president, fill(vacancies)) :- recess(senate).
 power(president, grant(commissions)) :- recess(senate).
-/*Commisions shall expire*/
+
 
 commanderInChiefOfArmy(president).
 commanderInChiefOfNavy(president).
 commanderInChiefOfMilitia(president) :- inService(militia).
-
-
-/* Requieres opinion of respective Head of Departments*/
 
 power(principalOfficer(Department), advice(president)).
--- constitution.pl
+++ constitution.pl
@@ -232,6 +232,27 @@ needsCongressConsent(Action) :- member(Action,needConsentOfCongress).
 
 
 /* Section 2 */
+power(president, grant(reprieve(offenseAgainstUS))).
+power(president, grant(pardon(offenseAgainstUS))).
+
+adviceAndConsent(senate).
+power(president, makeTreaties) :- adviceAndConsent(senate).
+power(president, appoint(nominate(X))) :-
+    member(X, [ambassador, minister, consul, judge(supremeCourt)]),
+    adviceAndConsent(senate).
+
+power(president, appoint(officers)).
+
+recess(senate).
+power(president, fill(vacancies)) :- recess(senate).
+power(president, grant(commissions)) :- recess(senate).
+
+
+commanderInChiefOfArmy(president).
+commanderInChiefOfNavy(president).
+commanderInChiefOfMilitia(president) :- inService(militia).
+
+power(principalOfficer(Department), advice(president)).
 
 
 /* Section 3 */
