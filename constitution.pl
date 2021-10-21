60c1f250795894f8d03aac3e743439c08149f55d
sarthag <asarthak2002@gmail.com>
2021-10-22 00:42:04 +0530

    amendment 1 done

--- constitution.pl
+++ constitution.pl
@@ -322,6 +322,11 @@ powerOfPunishment(treason, congress).
 /* Amendments */
 
 /* Amendment 1 */
+unRevokableRights ([freedom(speech),freedom(press),right(peacably(assemble)),petition(redress(grievances))]).
+newLaw(Right) :- not(member(Right,unRevokableRights)).
+
+/* Above statement checks whether the new law  
+takes away the unrevokable rights */
 
 
 /* Amendment 2 */
