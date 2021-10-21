e73dfc66f4f93595b4c1dc7ec51803e48f221333
sarthag <asarthak2002@gmail.com>
2021-10-22 00:24:18 +0530

    Article 3 done

--- Constitution Code/Article3/Section3.pl
+++ Constitution Code/Article3/Section3.pl
@@ -1,7 +1,8 @@
 treason(Actor1,unitedStates) :- 
-levyWar(Actor1, unitedStates);
-giveAidAndComfort(Actor1,Actor2), enemy(Actor2,unitedStates).
+    levyWar(Actor1, unitedStates);
+    giveAidAndComfort(Actor1,Actor2), enemy(Actor2,unitedStates).
+
 convicted(treason(Actor,unitedStates)) :-
-testimony((Witness1, Act1),(Witness2, Act1));
-confession(Actor,Act).
+    testimony((Witness1, Act1),(Witness2, Act1));
+    confession(Actor,Act).
 powerOfPunishment(treason, congress).
--- constitution.pl
+++ constitution.pl
@@ -240,17 +240,54 @@ needsCongressConsent(Action) :- member(Action,needConsentOfCongress).
 
 
 
-
 /* Article 3 */
 
 /* Section 1 */
-
+judicialPowers :- congressOrdained(Court).
+congressOrdained(supremeCourt).
+holdOffice(Judge) :- behaviour(Judge,good), tenure(Judge).
+compensation(Judge) :- holdOffice(Judge).
 
 /* Section 2 */
+judicialPowersExtent :- caseOf1; caseOf2; caseOf3; caseOf4.
+caseOf1 :- 
+  law(constitution);
+  equity(constitution);
+  law(unitedStates);
+  treaty(unitedStates,X).
+caseOf2 :- 
+  ambassador;
+  publicMinister;
+  consul.
+caseOf3 :-
+  admiraltyJurisdiction;
+  maritimeJurisdiction.
+caseOf4 :- controversy1(X,Y), controversy2(X,Y).
+controversy1(X,Y) :- 
+  controversy1(state(State1),state(State2));
+  controversy1(state(State1),citizen(State2));
+  controversy1(foerignState, state(State1).
+ controversy2(X,Y) :- 
+    controversy2(citizen(State1),citizen(State2));
+    controversy2(citizen1(State1),citizen2(State1), 
+    claimLandUnderGrant(citizen1(State1),State2),
+    claimLandUnderGrant(citizen1(State1),State3);
+    controversy2(foreignState, citizen(State1)).
+originalJurisdiction(supremeCourt) :- caseOf1; controversy1(X,Y).
+appellateJurisdiction(supremeCourt) :- caseOf2; caseOf3; controversy2(X,Y).
+
+trailByJury(Accused,State) :- crime(Accused,State).
 
 
 /* Section 3 */
+treason(Actor1,unitedStates) :- 
+    levyWar(Actor1, unitedStates);
+    giveAidAndComfort(Actor1,Actor2), enemy(Actor2,unitedStates).
 
+convicted(treason(Actor,unitedStates)) :-
+    testimony((Witness1, Act1),(Witness2, Act1));
+    confession(Actor,Act).
+powerOfPunishment(treason, congress).
 
 
 
