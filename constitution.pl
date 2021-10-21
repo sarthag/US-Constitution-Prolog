cf5ea5fd39bd6139eebd2198c6591127514d4d01
Shaz Furniturewala <72245851+shortguyshaz@users.noreply.github.com>
2021-10-22 03:44:03 +0530

    Update constitution.pl

--- constitution.pl
+++ constitution.pl
@@ -559,6 +559,11 @@ not() keyword sus */
 
 /* Amendment 10 */
 
+power(stateOfUS(Y), X) :-
+    not(delegated(X, unitedStates, constitution)),
+    not(prohibited(X, stateOfUS(Y), constitution)).
+    
+*/ will throw error for power(X,Y), will not for power(congress, Y) */
 
 /* Amendment 11 */
 
