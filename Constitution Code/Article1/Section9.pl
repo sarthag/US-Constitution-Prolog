1bd17f3b1b3a6e2a9a07af66775f300c199eaeaf
EpichilFire3 <91728278+EpichilFire3@users.noreply.github.com>
2021-10-22 02:40:30 +0530

    Update Section9.pl

--- Constitution Code/Article1/Section9.pl
+++ Constitution Code/Article1/Section9.pl
@@ -3,4 +3,17 @@ importation(X,Y,Z) :- migration(X,Y,Z).
 legal(Functor,Year).
 legal(migration(State1,State2,Person), Year) :- Year<1808.
 tax(migration(State1,State2,Person), Amount) :- Amount<10.
-writ(habeusCorpus
+not(writ(habeusCorpus)) :- 
+	case(rebellion);
+	case(invasion).
+not(pass(billOfAttainder)).
+not(pass(exFactoLaw)).
+capitation(tax) :-
+	tax is PropConst*Census.
+not(tax(ExportArea,article)) :- ExportState=stateOfUS(X).
+revenue(port(stateOfUS(X))).
+not(tax(vessel(State1,State2)),State2).
+not(titleOfNobility(unitedStates)).
+notAccept([present,emolument,office,title],person(X),[king,prince,foreignState]) :-
+	noConsent(congress),
+	(X=holding(officeOfProfit);X=holding(Trust)).
