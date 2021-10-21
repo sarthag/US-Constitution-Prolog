e367e0c411e2cf9d3d45fe52e008c85686a8724a
Shaz Furniturewala <72245851+shortguyshaz@users.noreply.github.com>
2021-10-22 01:00:18 +0530

    Update article-7.pl

--- Constitution Code/Article7/article-7.pl
+++ Constitution Code/Article7/article-7.pl
@@ -1,49 +1,51 @@
 establishmentOfConstitution(nineStates) :-
     ratification(nineStates).
-dateOfRatification(17,09,1787).
 
-witness(stateOfUs(newHampshire), johnLangdon).
-witness(stateOfUs(newHampshire), nicholasGilman).
-witness(stateOfUs(massachusetts), nathanielGorham).
-witness(stateOfUs(connecticut), wmSamlJohnson).
-witness(stateOfUs(connecticut), rogerSherman).
-witness(stateOfUs(connecticut), rogerSherman).
-witness(stateOfUs(connecticut), rogerSherman).
-witness(stateOfUs(newYork), alexanderHamilton).
-witness(stateOfUs(newJersey), wilLivingston).
-witness(stateOfUs(newJersey), davidBrearley).
-witness(stateOfUs(newJersey), wmPaterson).
-witness(stateOfUs(newJersey), jonaDayton).
-witness(stateOfUs(pennsylvania), bFranklin).
-witness(stateOfUs(pennsylvania), thomasMifflin).
-witness(stateOfUs(pennsylvania), robtMorris).
-witness(stateOfUs(pennsylvania), geoClymer).
-witness(stateOfUs(pennsylvania), thosFitzSimons).
-witness(stateOfUs(pennsylvania), jaredIngersoll).
-witness(stateOfUs(pennsylvania), jamesWilson).
-witness(stateOfUs(pennsylvania), gouvMorris).
-witness(stateOfUs(delaware), geoRead).
-witness(stateOfUs(delaware), gunningBedfordJun).
-witness(stateOfUs(delaware), johnDickinson).
-witness(stateOfUs(delaware), richardBassett).
-witness(stateOfUs(delaware), jacoBroom).
-witness(stateOfUs(maryland), jamesMcHenry).
-witness(stateOfUs(maryland), danOfStThosJenifer).
-witness(stateOfUs(maryland), danlCarroll).
-witness(stateOfUs(virginia), johnBlair).
-witness(stateOfUs(virginia), jamesMadisonJr).
-witness(stateOfUs(northCarolina), wmBlount).
-witness(stateOfUs(northCarolina), richdDobbsSpaight).
-witness(stateOfUs(northCarolina), huWilliamson).
-witness(stateOfUs(southCarolina), jRutledge).
-witness(stateOfUs(southCarolina), charlesCotesworthPinckney).
-witness(stateOfUs(southCarolina), charlesPinckney).
-witness(stateOfUs(southCarolina), pierceButler).
-witness(stateOfUs(georgia), williamFew).
-witness(stateOfUs(georgia), abrBaldwin).
-witness(stateOfUs(washington), secretaryWilliamJackson).
+dateOfRatification(17, 09, 1787).
+
+witness(stateOfUS(newHampshire), johnLangdon).
+witness(stateOfUS(newHampshire), nicholasGilman).
+witness(stateOfUS(massachusetts), nathanielGorham).
+witness(stateOfUS(connecticut), wmSamlJohnson).
+witness(stateOfUS(connecticut), rogerSherman).
+witness(stateOfUS(connecticut), rogerSherman).
+witness(stateOfUS(connecticut), rogerSherman).
+witness(stateOfUS(newYork), alexanderHamilton).
+witness(stateOfUS(newJersey), wilLivingston).
+witness(stateOfUS(newJersey), davidBrearley).
+witness(stateOfUS(newJersey), wmPaterson).
+witness(stateOfUS(newJersey), jonaDayton).
+witness(stateOfUS(pennsylvania), bFranklin).
+witness(stateOfUS(pennsylvania), thomasMifflin).
+witness(stateOfUS(pennsylvania), robtMorris).
+witness(stateOfUS(pennsylvania), geoClymer).
+witness(stateOfUS(pennsylvania), thosFitzSimons).
+witness(stateOfUS(pennsylvania), jaredIngersoll).
+witness(stateOfUS(pennsylvania), jamesWilson).
+witness(stateOfUS(pennsylvania), gouvMorris).
+witness(stateOfUS(delaware), geoRead).
+witness(stateOfUS(delaware), gunningBedfordJun).
+witness(stateOfUS(delaware), johnDickinson).
+witness(stateOfUS(delaware), richardBassett).
+witness(stateOfUS(delaware), jacoBroom).
+witness(stateOfUS(maryland), jamesMcHenry).
+witness(stateOfUS(maryland), danOfStThosJenifer).
+witness(stateOfUS(maryland), danlCarroll).
+witness(stateOfUS(virginia), johnBlair).
+witness(stateOfUS(virginia), jamesMadisonJr).
+witness(stateOfUS(northCarolina), wmBlount).
+witness(stateOfUS(northCarolina), richdDobbsSpaight).
+witness(stateOfUS(northCarolina), huWilliamson).
+witness(stateOfUS(southCarolina), jRutledge).
+witness(stateOfUS(southCarolina), charlesCotesworthPinckney).
+witness(stateOfUS(southCarolina), charlesPinckney).
+witness(stateOfUS(southCarolina), pierceButler).
+witness(stateOfUS(georgia), williamFew).
+witness(stateOfUS(georgia), abrBaldwin).
+witness(stateOfUS(washington), secretaryWilliamJackson).
 
 congressElects(Day1,Day2,Time,Place) :- ratification(nineStates).
+
 ratification(nineStates) :- 
         conventionOfDelegates(assent, State1) ,
         conventionOfDelegates(assent, State2) ,
@@ -54,6 +56,7 @@ ratification(nineStates) :-
         conventionOfDelegates(assent, State7) ,
         conventionOfDelegates(assent, State8) ,
         conventionOfDelegates(assent, State9).
-electorsAppointed(State1,Day1) :- congressElects(Day1,Day2,Time,Place).
-electorsVote(president, Day2) :- congressElects(Day1,Day2,Time,Place).
-convene(senators,representatives,Time,Place) :- congressElects(Day1,Day2,Time,Place).
+
+electorsAppointed(State1, Day1) :- congressElects(Day1, Day2, Time, Place).
+electorsVote(president, Day2) :- congressElects(Day1, Day2, Time, Place).
+convene(senators, representatives, Time, Place) :- congressElects(Day1, Day2, Time, Place).
