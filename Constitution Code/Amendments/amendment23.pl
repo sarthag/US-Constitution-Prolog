9e6a01cae78011d0fae5306c1f6e2b5352c1202e
EpichilFire3 <91728278+EpichilFire3@users.noreply.github.com>
2021-10-22 14:23:49 +0530

    Update amendment23.pl

--- Constitution Code/Amendments/amendment23.pl
+++ Constitution Code/Amendments/amendment23.pl
@@ -2,19 +2,19 @@
 sumHouses(districtOfColumbia, Sum) :-
 numSenators(districtOfColumbia, Senators), 
 numRepresentatives(districtOfColumbia, Representatives),
-(Sum = Senators + Representatives).
+(Sum is Senators + Representatives).
 
 numElectors(districtOfColumbia, Electors, MinElectors, Sum) :-
     numElectors(StateOfUS(MinELectors), MinElectors),
     sumHouses(districtOfColumbia, Sum),
     Sum < MinELectors,
-    Electors = Sum.
+    Electors is Sum.
 
 numElectors(districtOfColumbia, Electors, MinElectors, Sum) :-
     numElectors(StateOfUS(MinELectors), MinElectors),
     sumHouses(districtOfColumbia, Sum),
     Sum < MinELectors,
-    Electors = MinElectors.
+    Electors is MinElectors.
 
 /*Section 2*/
 power(congress,enforceByLegislation).
