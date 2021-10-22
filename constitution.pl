efe7b007e6ac57d864068c4550cd50e45bd7e60f
sarthag <asarthak2002@gmail.com>
2021-10-22 14:04:39 +0530

    amendment 20

--- constitution.pl
+++ constitution.pl
@@ -298,7 +298,7 @@ notAccept(Position ,person(holding(X)), From) :-
 	member(Position, [present, emolument, office, title]),
 	member(X, [officePost, trust]),
 	member(From, [king,prince,foreignState]).
-    
+
 
 /* Section 10 */
 tenderList([gold,silver]).
@@ -759,22 +759,61 @@ power(congress, enforceArticle(legislation)).
 
 /* Amendment 20 */
 
-/* Section 1 */
+/* SECTION 1 */
+
+end(term(president), date(1200, 20, january)).
+end(term(vicePresident), date(1200, 20, january)).
+end(term(senator), date(1200, 3, january)).
+end(term(representative), date(1200, 3, january)).
+
+begins(term(PresidentB)) :-
+    ended(term(PresidentA)).
+begins(term(VicePresidentB)) :-
+    ended(term(VicePresidentA)).
+begins(term(SenatorB)) :-
+    ended(term(SenatorA)).
+begins(term(RepresentativeB)) :-
+    ended(term(RepresentativeA)).
+
+/* SECTION 2 */
+
+assemble(congress, atleastOnce(year), date(1200, 3, january)) :-
+    not(appoint(differentDay)).
+
+/* SECTION 3 */
+
+become(president, vicePresident) :-
+    dead(presidentElect, beginningOfTerm).
+
+actingPresident(vicePresident, until(chosen(new(president)))) :-
+    not(chosen(president, before(beginningOfTerm)));
+    failed(qualify(presidentElect)).
+
+may(congress, byLaw(provide(case1))).
+
+case1 :- 
+    not(qualify(presidentElect)),
+    not(qualify(vicePresidentElect)).
+
+/* SECTION 4 */
+
+may(congress, byLaw(provide(case2))).
+
+case2 :- 
+    death(candidate(president, chosenBy(houseOfRepresentatives)));
+    death(candidate(vicePresident, chosenBy(senate))).
 
 
-/* Section 2 */
+/* SECTION 5 */
 
+takeEffect(section1, date(15, october), after(ratification(thisArticle))).
+takeEffect(section2, date(15, october), after(ratification(thisArticle))).
 
-/* Section 3 */
+/* SECTION 6 */
 
+operative(thisArticle) :-
+    ratified(amendment(constitution), threeFourths(states), withinYears(7)).
 
-/* Section 4 */
-
-
-/* Section 5 */
-
-
-/* Section 6 */
 
 
 /* Amendment 21 */
