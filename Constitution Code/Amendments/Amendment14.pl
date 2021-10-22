fbcfd21c75be07c1e0377c02807c8cefc0980f0a
half-anerd <74734120+half-anerd@users.noreply.github.com>
2021-10-22 13:10:24 +0530

    Update Amendment14.pl
    Final Amendment 14 
    Section2 might need modification based on Article 1 section2

--- Constitution Code/Amendments/Amendment14.pl
+++ Constitution Code/Amendments/Amendment14.pl
@@ -1,32 +1,33 @@
-*/ Section 1 */
+/* Section 1 */
 citizenOf(X, unitedStates) :- (born(X, unitedStates); naturalised(X, unitedStates)), jurisdiction(X, unitedStates).
 citizenOf(X, State) :- citizenOf(X, unitedStates), reside(X, State).
 not(law(state)) :- abridges(law(state), priviliges(citizen)).
 not(law(state)) :- abridges(law(state), immunities(citizen)).
-not(deprive(state, person, life)) :- not(dueProcess(law)).
-not(deprive(state, person, liberty)) :- not(dueProcess(law)).
-not(deprive(state, person, property)) :- not(dueProcess(law)).
-not(deprive(state, person, equalRights)) :- jurisdiction(state, person).
+not(deprive(state, X, life)) :- not(dueProcess(law)).
+not(deprive(state, X, liberty)) :- not(dueProcess(law)).
+not(deprive(state, X, property)) :- not(dueProcess(law)).
+not(deprive(state, X, equalRights)) :- jurisdiction(state, X).
 
-*/ Section 2 (mmodifies article 1 section2) */
+/* Section 2 (modifies article 1 section2) */
+basis(appointed(representative(stateOfUS(Y), X) :- X = numberOfVoters(stateOfUS(Y)).
 
-*/Section 3 */
-conditionsPreventingFromHoldingOffice :- 
-    (previously(takenOath(person, memberOfCongress)); previously(takenOath(person,officerOftheUnitedStates));previously(takenOath(person,memberOfStateLegislature)); previously(takenOath(person,executiveOfficer));previously(takenOath(person,judicialOfficer))),
-   (engaged(person, insurrection); engaged(person, rebellionAgainstConstitution); given(person, aidToUSEnemies)),
+/*Section 3 */
+conditionsPreventingFromHoldingOffice(X) :- 
+    (previously(takenOath(X, memberOfCongress)); previously(takenOath(X,officerOftheUnitedStates));previously(takenOath(X,memberOfStateLegislature)); previously(takenOath(X,executiveOfficer));previously(takenOath(X,judicialOfficer))),
+   (engaged(X, insurrection); engaged(X, rebellionAgainstConstitution); given(X, aidToUSEnemies)),
    (not(vote(removeDisability, twoThirds(senate))), not(vote(removeDisability, twoThirds(houseOfRepresentatives)))).
-not(become(person, senator)) :- 
-    conditionsPreventingFromHoldingOffice.   
-not(become(person, representativeInCongress)) :- 
-    conditionsPreventingFromHoldingOffice.
-not(become(person, electorOfPresidentAndVicePresident)) :-
-   conditionsPreventingFromHoldingOffice.
-not(hold(person, civilOffice(unitedStates))) :-
-     conditionsPreventingFromHoldingOffice.  
-not(hold(person, militaryOffice(unitedStates))) :-
-    conditionsPreventingFromHoldingOffice. 
+not(become(X, senator)) :- 
+    conditionsPreventingFromHoldingOffice(X).   
+not(become(X, representativeInCongress)) :- 
+    conditionsPreventingFromHoldingOffice(X).
+not(become(X, electorOfPresidentAndVicePresident)) :-
+   conditionsPreventingFromHoldingOffice(X).
+not(hold(X, civilOffice(unitedStates))) :-
+     conditionsPreventingFromHoldingOffice(X).  
+not(hold(X, militaryOffice(unitedStates))) :-
+    conditionsPreventingFromHoldingOffice(X). 
 
-*/ Section 4 */
+/* Section 4 */
 not(questioned(validity(publicDebt(unitedStates, authorizedByLaw)))).
 not(questioned(validity(publicDebt(unitedStates, authorizedByLaw)))):- 
     incurred(debt, paymentOfPensionsAndBounties(supressing(insurrectionOrRebellion(against(unitedStates))))).
@@ -35,5 +36,5 @@ not(pay(incurred(debt, insurrectionOrRebellion(against(unitedStates))))).
 not(pay(claim(loss))).
 not(pay(claim(emancipationOfSlave))).
 
-*/ Section 5 */
+/* Section 5 */
 power(congress, enforceArticle(legislation)).
