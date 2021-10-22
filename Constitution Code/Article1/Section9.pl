e9e8fd6f9f4aa96fd1ee9a67caea0b83dcef2cd0
half-anerd <74734120+half-anerd@users.noreply.github.com>
2021-10-22 13:49:52 +0530

    Update Section9.pl

--- Constitution Code/Article1/Section9.pl
+++ Constitution Code/Article1/Section9.pl
@@ -1,19 +1,22 @@
-migration(State1,State2,Person).
+migration(State1,State2,Person) :- stateOfUS(State1), stateOfUS(State2).
 importation(X,Y,Z) :- migration(X,Y,Z).
 legal(Functor,Year).
 legal(migration(State1,State2,Person), Year) :- Year<1808.
 tax(migration(State1,State2,Person), Amount) :- Amount<10.
-not(writ(habeusCorpus)) :- 
+suspeneded(writ(habeusCorpus)) :- 
 	case(rebellion);
 	case(invasion).
 not(pass(billOfAttainder)).
 not(pass(exFactoLaw)).
 capitation(tax) :-
-	tax is PropConst*Census.
-not(tax(ExportArea,article)) :- ExportState=stateOfUS(X).
+	proprtion(tax, census).
+not(tax(ExportState,article)) :- stateOfUS(ExportState).
 revenue(port(stateOfUS(X))).
-not(tax(vessel(State1,State2)),State2).
+not(tax(vessel(State1,State2)),State2) :- stateOfUS(State1), stateOfUS(State2).
 not(titleOfNobility(unitedStates)).
-notAccept([present,emolument,office,title],person(X),[king,prince,foreignState]) :-
-	noConsent(congress),
-	(X=holding(officeOfProfit);X=holding(Trust)).
+noConsent(congress, X). /* extra fact added */
+notAccept(Offering,person(X), ForeignPower) :-
+    member(Offfering, [present,emolument,office,title]),
+    member(ForeignPower, [king,prince,foreignState]),
+    noConsent(congress, Offering),
+    (X=holding(officeOfProfit); X=holding(trust)).
