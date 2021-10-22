b010af3fcb64068edffd7a5a5d4285d95c82c643
Shaz Furniturewala <72245851+shortguyshaz@users.noreply.github.com>
2021-10-22 14:29:52 +0530

    Update amendment23.pl

--- Constitution Code/Amendments/amendment23.pl
+++ Constitution Code/Amendments/amendment23.pl
@@ -1,20 +1,20 @@
 /*Section 1*/
 sumHouses(districtOfColumbia, Sum) :-
-numSenators(districtOfColumbia, Senators), 
-numRepresentatives(districtOfColumbia, Representatives),
-(Sum is Senators + Representatives).
-
-numElectors(districtOfColumbia, Electors, MinElectors, Sum) :-
-    numElectors(StateOfUS(MinELectors), MinElectors),
-    sumHouses(districtOfColumbia, Sum),
-    Sum < MinELectors,
-    Electors is Sum.
-
-numElectors(districtOfColumbia, Electors, MinElectors, Sum) :-
-    numElectors(StateOfUS(MinELectors), MinElectors),
-    sumHouses(districtOfColumbia, Sum),
-    Sum < MinELectors,
-    Electors is MinElectors.
-
-/*Section 2*/
-power(congress,enforceByLegislation).
+    numSenators(districtOfColumbia, Senators), 
+    numRepresentatives(districtOfColumbia, Representatives),
+    (Sum is Senators + Representatives).
+    
+    numElectors(districtOfColumbia, Electors, MinElectors, Sum) :-
+        numElectors(stateOfUS(MinELectors), MinElectors),
+        sumHouses(districtOfColumbia, Sum),
+        Sum < MinELectors,
+        Electors is Sum.
+    
+    numElectors(districtOfColumbia, Electors, MinElectors, Sum) :-
+        numElectors(stateOfUS(MinELectors), MinElectors),
+        sumHouses(districtOfColumbia, Sum),
+        Sum < MinELectors,
+        Electors is MinElectors.
+    
+    /*Section 2*/
+power(congress, enforceArticle(legislation)).
